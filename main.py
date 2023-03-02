bdccac402a135d3fb06bec98bc9774c16734e590
Olubusolami-R <olubusolami.sogunle@stu.cu.edu.ng>
2023-03-02 16:29:34 +0100

    map word to index

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 from data import data as docs
 
-print(docs)
+#step 1 : map word to integer
+idx_2_word={}
+word_2_idx={}
+temp=[]
+i=1
 
+for doc in docs:
+    for word in doc.split():
+        if word not in temp:
+            temp.append(word)
+            idx_2_word[i]=word
+            word_2_idx[word]=i
+            i+=1
+
+print("idx_2_word")
+print(idx_2_word)
+
+print("word_2_idx")
+print(word_2_idx)
+
+#step2
