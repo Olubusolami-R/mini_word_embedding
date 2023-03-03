42b1180709a4d24d6cd4d81a6e0f2bf1e2f58ab9
Olubusolami-R <olubusolami.sogunle@stu.cu.edu.ng>
2023-03-03 20:40:30 +0100

    resolve tf issue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from data import data as docs
-
+import tensorflow as tf
 #step 1 : map word to integer
 idx_2_word={}
 word_2_idx={}
@@ -16,8 +16,24 @@ for doc in docs:
 
 print("idx_2_word")
 print(idx_2_word)
-
+print()
 print("word_2_idx")
 print(word_2_idx)
 
-#step2
+
+#step2 : one - hot encoding
+vocab_size=25
+def one_hot_map(doc):
+    x=[]
+    for word in doc.split():
+        x.append(word_2_idx[word])
+    return x
+
+encoded_docs=[one_hot_map(d) for d in docs]
+print()
+print(encoded_docs)
+
+# max_len=10
+# padded_docs=tf.keras.utils.pad_sequences(encoded_docs,maxlen=max_len,padding='post')
+# padded_docs
+print(tf.__version__)
